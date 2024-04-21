615f0adb671f424b638659363f36ba6488e0d879
whypain <127675580+whypain@users.noreply.github.com>
2024-04-20 20:22:37 -0400

    TouchToLook

---
+++ Assets/Scripts/TouchToLook.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchToLook : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed = 5f;
+    [SerializeField] private float _MouseZDist = 10f;
+
+    private Vector3 mousePos;
+    private Camera mainCamera;
+    private Transform localTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localTrans = GetComponent<Transform>();
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // get the world position of the mouse
+            mousePos = mainCamera.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, _MouseZDist) );
+
+            // determine the target rotation
+            Quaternion targetRot = Quaternion.LookRotation(mousePos - localTrans.position);
+
+            // smoothly rotate towards the target point.
+            localTrans.rotation = Quaternion.Slerp(localTrans.rotation, targetRot, _rotateSpeed * Time.deltaTime);
+        }
+    }
+}
